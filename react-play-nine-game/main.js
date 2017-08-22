d3488d1094599be70a4ad460d9fd5c6a8e1cc849
tienpham94 <r3dsky02@yahoo.com.vn>
2017-08-22 21:47:02 +0300

    Starting play nine game

---
+++ react-play-nine-game/main.js
@@ -0,0 +1,20 @@
+
+
+const Stars = () => {
+  
+}
+
+const Game = () => {
+
+}
+
+class App extends React.Component{
+  render(){
+    return(
+      <Game />
+      <div> ... </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
